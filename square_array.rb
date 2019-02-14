836736f6b47da3a0ffbc36ddca741dca38a62dd5
HARRY BHATT <bob@brahmingate.co.uk>
2019-02-14 13:11:38 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,14 +1,3 @@
-def square_array_old(array)
-tempArry = [] 
-  
-array.each do |element| 
-  tempArry.push(element**2)
-end
-  return tempArry    
-    
-  # your code here
-end
-
 
 def square_array(array)
   
